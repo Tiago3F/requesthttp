b25e66c676d6e717f12b2fde2d9b5b57a9fb0789
tiago <tiago.moreira@timebrisa.com.br>
2022-05-04 13:58:34 -0300

    feat: :sparkles: Ajustando rotas

--- src/app/cursos/cursos-routing.module.ts
+++ src/app/cursos/cursos-routing.module.ts
@@ -1,7 +1,12 @@
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: CursosListaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CursosListaComponent
+  ],
   imports: [
     CommonModule,
     CursosRoutingModule
