40daf3634cf7cdfeff9ee33ce6ca71316393e0b7
tiago <tiago.moreira@timebrisa.com.br>
2022-05-06 15:00:38 -0300

    feat: :sparkles: Validação de formulário

--- src/app/cursos/cursos-form/cursos-form.component.ts
+++ src/app/cursos/cursos-form/cursos-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cursos-form',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosFormComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup
+  submitted = false
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+     this.form = this.fb.group({
+       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]]
+     })
   }
 
+  hasError(field: string) {
+    return this.form.get(field)?.errors
+  }
+
+  onSubmit() {
+    this.submitted = true
+    console.log(this.form.value)
+    if (this.form.valid) {
+      console.log('submit')
+    }
+  }
+
+  onCancel() {
+    this.submitted = false
+    this.form.reset()
+    // console.log('onCancel')
+  }
 }
